4a2065041889abcb1594042b3c0113c7e2028f1b
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-10 13:19:25 +0100

    body image added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Navigation from "./components/NavigationComponent/Navigation";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <Navigation />
+      <HomePage />
     </>
   );
 }
