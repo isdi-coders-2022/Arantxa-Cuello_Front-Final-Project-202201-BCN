26020c7511d33ce20660fa0113c7bf443e4fb22b
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-10 16:08:04 +0100

    session card added

--- src/components/SessionCardComponent/SessionCard.tsx
+++ src/components/SessionCardComponent/SessionCard.tsx
@@ -1,25 +1,24 @@
 import { faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Session from "../../types/Session";
 
-const SessionCard = ({ session, modifyCard, deleteCard }) => {
+interface SessionCardProps {
+  session: Session;
+}
+//faltan las acciones de modificar y borrar la session
+const SessionCard = ({
+  session: { title, date, comment },
+}: SessionCardProps): JSX.Element => {
   return (
     <li>
-      <h1>{session.title}</h1>
-      <p>{session.date}</p>
+      <h1>{title}</h1>
+      <p>{date}</p>
       <div>
-        <p>{session.comment}</p>
+        <p>{comment}</p>
       </div>
       <div>
-        <FontAwesomeIcon
-          icon={faPen}
-          className="modify_card"
-          onClick={() => modifyCard.id}
-        />
-        <FontAwesomeIcon
-          icon={faTrashCan}
-          className="delete_card"
-          onClick={() => deleteCard.id}
-        />
+        <FontAwesomeIcon icon={faPen} className="modify_card" />
+        <FontAwesomeIcon icon={faTrashCan} className="delete_card" />
       </div>
     </li>
   );
