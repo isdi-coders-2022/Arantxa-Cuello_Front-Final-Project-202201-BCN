a158dcf6781ae06377e6766a358f70a5c898423c
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-11 11:01:12 +0100

    handlers created

---
+++ src/redux/mocks/handlers.ts
@@ -0,0 +1,25 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get(
+    `${process.env.REACT_APP_API_MINDFULNESS}allsessions`,
+    (req, res, ctx) =>
+      res(
+        ctx.status(200),
+        ctx.json({
+          messages: [
+            {
+              title: "hello",
+              comment: "hola",
+              date: "today",
+            },
+            {
+              title: "bye",
+              comment: "adios",
+              date: "today",
+            },
+          ],
+        })
+      )
+  ),
+];
