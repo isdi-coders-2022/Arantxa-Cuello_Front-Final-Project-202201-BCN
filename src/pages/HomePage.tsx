a03ce5521d00b82196094c57ef81913e06dd7f19
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-12 23:19:01 +0100

    home page styled

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,20 +2,58 @@ import Navigation from "../components/NavigationComponent/Navigation";
 import meditation from "../images/meditation.png";
 import styled from "styled-components";
 import Footer from "../components/FooterComponent/Footer";
+import { primary } from "../styles/globalStyles";
 
-const Body = styled.section``;
+const Body = styled.article`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  padding-top: 120px;
+  .body-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  h1 {
+    font-weight: normal;
+    font-size: 2em;
+    color: ${primary};
+    text-align: center;
+  }
+  .picture {
+    display: block;
+  }
+  .headlines {
+    margin-left: 38px;
+    margin-top: 18px;
+    max-width: 600px;
+    width: 100%;
+  }
+  @media screen (max-width: 600px) {
+    h1 {
+      display: inline-block;
+      text-align: center;
+    }
+  }
+`;
 const HomePage = () => {
   return (
     <>
       <Navigation />
-      <Body className="body_container">
-        <img
-          src={meditation}
-          alt="meditation woman"
-          className="body_container_image"
-          height={459}
-          width={574}
-        />
+      <Body>
+        <div className="body-container">
+          <div className="picture">
+            <img
+              src={meditation}
+              alt="meditation woman"
+              className="body_container_image"
+              height={459}
+              width={574}
+            />
+          </div>
+
+          <h1>Welcome to Mindfulness, your meditation space.</h1>
+        </div>
       </Body>
       <Footer />
     </>
