d9a69f89756dfa94e4f31e381b10fc35d44047ac
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-12 20:59:53 +0100

    add set time out to test

--- src/redux/thunks/sessionsThunks.test.ts
+++ src/redux/thunks/sessionsThunks.test.ts
@@ -4,7 +4,7 @@ import { loadSessionsThunk } from "./sessionsThunks";
 describe("Given a loadSessionsThunk function", () => {
   describe("When it's called", () => {
     test("Then it should dispatch a function", async () => {
-      jest.setTimeout(90000);
+      jest.setTimeout(900000);
 
       const dispatch = jest.fn();
 
