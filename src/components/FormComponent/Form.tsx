82194f52a976a41fe10aadf5b83b92bd5aed532c
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-16 12:20:01 +0100

    css added to form

--- src/components/FormComponent/Form.tsx
+++ src/components/FormComponent/Form.tsx
@@ -1,6 +1,8 @@
 import { useState, ChangeEvent, SyntheticEvent } from "react";
 import { useDispatch } from "react-redux";
+import styled from "styled-components";
 import { createSessionThunk } from "../../redux/thunks/sessionsThunks";
+import { primary } from "../../styles/globalStyles";
 
 const SessionForm = (): JSX.Element => {
   const initialData = {
@@ -15,7 +17,7 @@ const SessionForm = (): JSX.Element => {
   const changeTitle = (event: ChangeEvent<HTMLInputElement>) => {
     setSession({ ...session, title: event.target.value });
   };
-  const changeComment = (event: ChangeEvent<HTMLInputElement>) => {
+  const changeComment = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setSession({ ...session, comment: event.target.value });
   };
   const changeIframe = (event: ChangeEvent<HTMLInputElement>) => {
@@ -34,46 +36,66 @@ const SessionForm = (): JSX.Element => {
   const resetForm = () => {
     setSession(initialData);
   };
+  const Form = styled.main`
+    padding-top: 120px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    h1 {
+      padding: 10px;
+      color: ${primary};
+      font-size: 20px;
+      font-weight: normal;
+    }
+  `;
 
   return (
-    <div className="form-container">
+    <Form className="form-container">
       <form noValidate autoComplete="off" onSubmit={submitSession}>
         <h1>Welcome! You can create your own meditation session here.</h1>
-        <label htmlFor="text">Title:</label>
-        <input
-          type="text"
-          id="text"
-          value={session.title}
-          onChange={changeTitle}
-        />
-        <label htmlFor="text">Date:</label>
-        <input
-          type="date"
-          id="date"
-          value={session.date}
-          onChange={changeDate}
-        />
-        <label htmlFor="text">Comments:</label>
-        <input
-          type="text"
-          id="text"
-          value={session.comment}
-          onChange={changeComment}
-        />
-        <label htmlFor="text">Please enter your media url</label>
-        <input
-          type="text"
-          id="text"
-          value={session.iFrame}
-          onChange={changeIframe}
-        />
+        <div>
+          <label htmlFor="text">Title</label>
+          <input
+            type="text"
+            id="text"
+            value={session.title}
+            onChange={changeTitle}
+          />
+        </div>
+        <div>
+          <label htmlFor="text">Date</label>
+          <input
+            type="date"
+            id="date"
+            value={session.date}
+            onChange={changeDate}
+          />
+        </div>
+        <div>
+          <label htmlFor="text">Comments</label>
+          <textarea
+            id="text"
+            value={session.comment}
+            onChange={changeComment}
+          />
+        </div>
+        <div>
+          <label htmlFor="text">Enter your media url</label>
+          <input
+            type="text"
+            id="text"
+            value={session.iFrame}
+            onChange={changeIframe}
+          />
+        </div>
         <div>
           <button className="form-button" type="submit">
             Send
           </button>
         </div>
       </form>
-    </div>
+    </Form>
   );
 };
 
