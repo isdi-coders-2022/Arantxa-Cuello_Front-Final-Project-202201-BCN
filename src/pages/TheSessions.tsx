a74f74f4912f3419e67af6ed7c9982df90b8b7bb
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-17 10:40:00 +0100

    loader added

--- src/pages/TheSessions.tsx
+++ src/pages/TheSessions.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import { RootState } from "../redux/reducers";
 import {
   deleteSessionThunk,
@@ -7,13 +8,18 @@ import {
 } from "../redux/thunks/sessionsThunks";
 import styled from "styled-components";
 import SessionCard from "../components/SessionCardComponent/SessionCard";
+import ClipLoader from "react-spinners/ClipLoader";
+import { primary } from "../styles/globalStyles";
 
 const TheSessions = (): JSX.Element => {
   const sessions = useSelector((state: RootState) => state.sessions);
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    setIsLoading(true);
     dispatch(loadSessionsThunk);
+    setIsLoading(false);
   }, [dispatch]);
 
   const deleteSession = (id: string) => {
@@ -23,15 +29,19 @@ const TheSessions = (): JSX.Element => {
   return (
     <>
       <MainSection>
-        <ul>
-          {sessions.map((session) => (
-            <SessionCard
-              session={session}
-              key={session.id}
-              actionOnClick={() => deleteSession(session.id)}
-            />
-          ))}
-        </ul>
+        {isLoading ? (
+          <ClipLoader color={primary} size={150} />
+        ) : (
+          <ul>
+            {sessions.map((session) => (
+              <SessionCard
+                session={session}
+                key={session.id}
+                actionOnClick={() => deleteSession(session.id)}
+              />
+            ))}
+          </ul>
+        )}
       </MainSection>
     </>
   );
