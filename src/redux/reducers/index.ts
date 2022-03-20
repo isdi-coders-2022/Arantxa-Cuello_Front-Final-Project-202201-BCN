0b70a9dac4810d5f99257940cca7083c184c39c4
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-20 17:59:05 +0100

    user reducer added to index

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import store from "../store";
 import oneSessionReducer from "./oneSessionReducer";
 import sessionReducer from "./sessionReducer";
+import userReducer from "./userReducer";
 
 export const rootReducer = combineReducers({
   sessions: sessionReducer,
   currentSession: oneSessionReducer,
+  user: userReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
